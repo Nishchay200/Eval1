8bd0e8cb41276d13f031f178d0b92376cc109dc2
Nishchay200 <22103284@mail.jiit.ac.in>
2024-08-31 10:28:37 +0530

    Create ques.py

---
+++ ques.py
@@ -0,0 +1,11 @@
+class task :
+    info={};
+    def addtask(taskname,taskid,taskstatus):
+        info[name]["nam1"]=taskname;
+        info[ID]["id1"]=taskid;
+
+name1=input("enter task name");
+id1=input("enter task id");
+status1=input("enter status");
+t=task();
+t.addtask(name1,id1,status1);
